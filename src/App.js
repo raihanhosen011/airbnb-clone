5746e750a337b49391b6d3da0b53bf468dfe2d91
raihanhosen011 <raihanhosen011@gmail.com>
2021-10-13 14:19:47 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import SearchResult from "./pages/SearchResult";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-import { Switch,Route } from 'react-router-dom'
-import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <div className='airbnb' >
           <Header/>  
-          <Switch>
-            <Route path='/' component={Home}  exact/>
-            <Route path='/search' component={SearchResult}/>
-          </Switch>
+            <Switch>
+              <Route path='/' component={Home}  exact/>
+              <Route path='/search' component={SearchResult}/>
+            </Switch>
           <Footer/>
       </div>
     </>
